a6c7c2a387c0f1cbeaaca0a3932dd7dad8da2cd4
andremarques94 <andremarques94@users.noreply.github.com>
2024-04-28 21:49:09 +0000

    deploy: dec54f99f69cd7bc4a0febc27e514e95ae88f7fb

---
+++ js/pages/test.js
@@ -0,0 +1,4 @@
+function e(T){T.style="width:100%; height:100%;display:flex;justify-content:center;align-items:center;",T.innerHTML=`<div style="text-align: center;">
+    <h1>TESTTTTTTTTTTTTTTTTTTTTTTTTA</h1>
+    <h2><code>TESTTTTTTTTTTTTTTTTTTTTTTTTA</code></h2>
+    </div>`}export{e as render};
