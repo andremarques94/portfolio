47e7069dcf4da2f667c033d83fe8c064e25bebf7
NOZK <andre.marques@academiadecodigo.org>
2024-04-29 11:50:53 +0100

    Add linkedin to img

--- js/components/header-navbar.js
+++ js/components/header-navbar.js
@@ -9,7 +9,7 @@ export function navbar() {
               <li><a href="#/portfolio">Portfolio</a></li>
               <li><a href="#/contact">Contact</a></li>
             </ul>
-            <img class="nav-logo" src="./assets/me.jpg" alt="André Marques">
+            <a href="https://www.linkedin.com/in/andrelourencomarques" target="_blank"><img class="nav-logo" src="./assets/me.jpg" alt="André Marques"></a>
           </div>`;
 
     return nav;
