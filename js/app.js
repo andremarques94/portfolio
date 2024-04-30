ff83bb0f445497e7d201da3765f5c59256f21432
andremarques94 <andremarques94@users.noreply.github.com>
2024-04-30 11:49:35 +0000

    deploy: 7cbe7e45fccdbceb44edf924c3af92afe5f074e6

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-function r(){let e=document.createElement("nav");return e.className="navbar",e.innerHTML=`
+function r(){let n=document.createElement("nav");return n.className="navbar",n.innerHTML=`
           <div class="nav-container">
             <ul class="nav-links">
-              <li><a href="#/home">Home</a></li>
+              <li><a href="#/">Home</a></li>
               <li><a href="#/about">About</a></li>
               <li><a href="#/portfolio">Portfolio</a></li>
               <li><a href="#/contact">Contact</a></li>
             </ul>
             <a href="https://www.linkedin.com/in/andrelourencomarques" target="_blank"><img class="nav-logo" src="./assets/me.jpg" alt="Andr\xE9 Marques"></a>
-          </div>`,e}var t;async function l(e,n){(await import(`${new URL(".",import.meta.url).href}pages/${n}.js`)).render(e)}function u({path:e,page:n}){t.currentPath.path=e,t.currentPath.page=n}function h({page:e}){let n=document.querySelector("#app"),a=document.createElement("div");a.appendChild(r()),n.innerHTML="",n.appendChild(a),l(a,e)}function i(e){if(e===t.currentPath.path)return;let n=Object.keys(t).find(o=>t[o].path===e),a=t[n]||t.home;u(a),h(a)}function c(e){return new URL(e).hash.slice(1)}function d(){addEventListener("hashchange",function(e){let n=c(e.newURL);i(n)})}function p(e){t=e,window.location.hash=window.location.hash||t.home.path,i(c(window.location.href)),d()}var s={init:p};window.addEventListener("DOMContentLoaded",async()=>{let n=await(await fetch("./js/routes.json")).json();s.init(n)});
+          </div>`,n}var t;async function l(n,e){(await import(`${new URL(".",import.meta.url).href}pages/${e}.js`)).render(n)}function u({path:n,page:e}){t.currentPath.path=n,t.currentPath.page=e}function h({page:n}){let e=document.querySelector("#app"),a=document.createElement("div");a.appendChild(r()),e.innerHTML="",e.appendChild(a),l(a,n)}function i(n){if(n===t.currentPath.path)return;let e=Object.keys(t).find(o=>t[o].path===n),a=t[e]||t.home;u(a),h(a)}function c(n){return new URL(n).hash.slice(1)}function d(){addEventListener("hashchange",function(n){let e=c(n.newURL);i(e)})}function p(n){t=n,window.location.hash=window.location.hash||t.home.path,i(c(window.location.href)),d()}var s={init:p};window.addEventListener("DOMContentLoaded",async()=>{let e=await(await fetch("./js/routes.json")).json();s.init(e)});
--- js/components/header-navbar.js
+++ js/components/header-navbar.js
@@ -1,7 +1,7 @@
 function n(){let a=document.createElement("nav");return a.className="navbar",a.innerHTML=`
           <div class="nav-container">
             <ul class="nav-links">
-              <li><a href="#/home">Home</a></li>
+              <li><a href="#/">Home</a></li>
               <li><a href="#/about">About</a></li>
               <li><a href="#/portfolio">Portfolio</a></li>
               <li><a href="#/contact">Contact</a></li>
