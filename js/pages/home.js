9c28a9b1f451e4f0f93bcd6529182869847aba3d
andremarques94 <andremarques94@users.noreply.github.com>
2024-05-01 21:37:12 +0000

    deploy: 7618039d2e728772addc64193b7a821a8720cbcf

--- js/pages/home.js
+++ js/pages/home.js
@@ -16,7 +16,7 @@ function s(e){let t=document.createElement("div");t.innerHTML=`
             <div class="relative h-64 sm:h-72 lg:h-full">
               <img
                 src="./assets/final-class.jpg"
-                alt="NOZK IMAGE!"
+                alt="NOZK class photo"
                 class="absolute inset-0 h-full w-full object-contain"
                 width="1200"
                 height="900"
